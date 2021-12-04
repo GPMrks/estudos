0230e645e1e9b54bdb7db9d48162e46dc5400b13
GPMrks <guilhermepereiramarques@hotmail.com>
2021-12-04 17:57:48 -0300

    Java files

---
+++ Java methods/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "GPMrks";
+        int age = 27;
+
+        hello(name, age);
+
+    }
+
+    static void hello(String parametro, int idade){
+
+        System.out.println("Hello " + parametro);
+        System.out.println("You are " + idade + " years!");
+
+    }
+
+}
