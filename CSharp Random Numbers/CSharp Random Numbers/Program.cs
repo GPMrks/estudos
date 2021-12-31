da2b5256118f5827876a210ec2152662e3a6f7fc
GPMrks <guilhermepereiramarques@hotmail.com>
2021-12-31 12:35:41 -0300

    New C# Files

---
+++ CSharp Random Numbers/CSharp Random Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Random_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
