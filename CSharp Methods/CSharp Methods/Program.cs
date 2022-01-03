46e7d01e649a3e21aeb1a361cf8a0f3a56f6c2f5
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-02 22:25:28 -0300

    New C# Files

---
+++ CSharp Methods/CSharp Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method  = performs a section of code, whenever it's called "invoked".
+            //           benefit = Let's us reuse code w/o writing it multiple times
+            //           Good practice is to capitalize method names
+
+            String name = "GPMrks";
+            int age = 27;
+
+            singHappyBirthday(name, age);
+
+            Console.ReadKey();
+        }
+
+        static void singHappyBirthday(String name, int age)
+        {
+
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear " + name + "!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("You are " + age + " years old!");
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ CSharp Return Keyword/CSharp Return Keyword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Return_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // return  = returns data back to the place where a method is invoked
+
+            double x;
+            double y;
+            double result;
+
+            Console.Write("Enter first number: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine("The result is " + result);
+
+            Console.ReadKey();
+        }
+
+        static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+    }
+}
