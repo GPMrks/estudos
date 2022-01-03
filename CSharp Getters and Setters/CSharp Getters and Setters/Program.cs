0023a9fdd50d9191ad1dfeb01a289df8b2d40150
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-03 13:40:25 -0300

    C# Getter and Setter

---
+++ CSharp Getters and Setters/CSharp Getters and Setters/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CSharp_Getters_and_Setters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //getters & setters = add security to fields by encapsulation
+            //                    They're accessors found within properties
+
+            // properties = combine aspects of both fields and methods (share name with a field)
+            // get accessor = used to return the property value
+            // set accessor = used to assign a new value
+            // value keyword = defines the value being assigned by the set (parameter)
+
+            Console.Write("What's the speed? ");
+            Car car = new Car(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("\nThe car is going at " + car.Speed + " km/h!\n");
+
+            Console.ReadKey();
+        }
+
+        class Car
+        {
+            private int speed;
+
+            public Car(int speed)
+            {
+                Speed = speed;
+            }
+
+            public int Speed
+            {
+                get { return speed; } // readable
+
+                set                   // writable
+                {
+
+                    if (value > 220)
+                    {      
+                        speed = 220;
+                        Console.WriteLine("\nThe car speed limit is " + speed + " km/h!");
+                    }
+                    else
+                    {
+                        speed = value;
+                    }
+                    
+                }
+            }
+
+        }
+
+    }
+}
