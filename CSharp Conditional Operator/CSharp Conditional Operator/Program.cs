b494211b94d962750360a8306125e29038a00f57
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-03 15:24:27 -0300

    New C# Files

---
+++ CSharp Conditional Operator/CSharp Conditional Operator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp_Conditional_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if a condition is true/false
+            //(condition) ? x : y
+
+            double temperature = 20;
+            String message;
+
+            /*
+            if(temperature >= 50)
+            {
+                message = "It's warm outside";
+            }
+            else
+            {
+                message = "It's cold outside";
+            }
+            */
+
+            message = (temperature >= 15) ? "It's warm outside" : "It's cold outside";
+
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp Exception Handling/CSharp Exception Handling/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CSharp_Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exception = errors that occur during execution
+
+            //        try     = try some code that is considered "dangerous"
+            //        catch   = catches and handles exceptions when they occur
+            //        finally = always executes regardless if exception is caught or not
+
+            int x;
+            int y;
+            double result;
+            bool right = true;
+
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter number 1: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter number 2: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+
+                    result = x / y;
+
+                    Console.WriteLine("Result: " + result);
+
+                    right = true;
+                }
+
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Enter only NUMBERS, please.\n");
+                    right = false;
+                }
+
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("You can't divide by zero!\n");
+                    right = false;
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.\n");
+                    right = false;
+                }
+            }
+            while (right == false);
+
+            Console.WriteLine("Thanks for visiting!");
+
+            Console.ReadKey();
+        }
+    }
+}
