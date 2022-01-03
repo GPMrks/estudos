6fbba4f18f73ef6397ce125c3e2f634115810f63
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-03 15:49:38 -0300

    New C# Files

---
+++ CSharp Classes/CSharp Classes/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Classes
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program!");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something!");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting!");
+        }
+
+    }
+}
---
+++ CSharp Classes/CSharp Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class = A bundle of related code.
+            //              Can be used as a blueprint to create objects (OOP)
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.ReadKey();
+        }
+    }
+
+    /*
+    class Messages
+    {
+        void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program!");
+        }
+
+        void Waiting()
+        {
+            Console.WriteLine("I am waiting for something!");
+        }
+
+        void Bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting!");
+        }
+    }
+    */
+
+}
