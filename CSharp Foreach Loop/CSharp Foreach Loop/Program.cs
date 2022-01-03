c6551c35845003847e813ad3590d537940a85ef8
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-02 23:12:49 -0300

    New C# files

---
+++ CSharp Foreach Loop/CSharp Foreach Loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp_Foreach_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            // in comparison to foreach
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
