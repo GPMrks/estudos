19db918a3e24a0a8ff3f68ad52c14b32bfe4fce4
GPMrks <guilhermepereiramarques@hotmail.com>
2022-01-04 17:31:09 -0300

    New Java Files

---
+++ Java Interface/src/Fish.java
@@ -0,0 +1,11 @@
+public class Fish implements Predator, Prey{
+    @Override
+    public void hunt() {
+        System.out.println("*This fish is hunting smaller fish*");
+    }
+
+    @Override
+    public void flee() {
+        System.out.println("*This fish is fleeing from bigger fish*");
+    }
+}
